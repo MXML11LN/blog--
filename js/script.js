297dd5835fb542db9f8ee725df434f8e4db40496
Max <78656849+MXML11LN@users.noreply.github.com>
2021-03-31 21:12:17 +0300

    started john-blog-project

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const menuIcon = document.querySelector(".menu__icon");
+if(menuIcon) {
+    const menuBody = document.querySelector(".menu__body");
+    menuIcon.addEventListener("click", function(e) {
+        document.body.classList.toggle("_lock");
+        menuIcon.classList.toggle("_active");
+        menuBody.classList.toggle("_active");
+    }); 
+}
